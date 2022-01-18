c7b62ab0ce917808d842f011b66eb09560760132
softdev1029 <softdev1029@outlook.com>
2022-01-18 17:32:50 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+
+interface IFormInput {
+  email: string;
+  firstName: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  firstName: yup.string().required().min(2).max(25),
+  password: yup.string().required().min(8).max(120),
+});
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    textAlign: "center",
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}));
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+
+  const { heading, submitButton } = useStyles();
+
+  const [json, setJson] = useState<string>();
+
+  const onSubmit = (data: IFormInput) => {
+    setJson(JSON.stringify(data));
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Typography className={heading} variant="h3">
+        Sign Up Form for Travel Syndication Technology
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          {...register("email")}
+          variant="outlined"
+          margin="normal"
+          label="Email"
+          helperText={errors.email?.message}
+          error={!!errors.email?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...register("firstName")}
+          variant="outlined"
+          margin="normal"
+          label="First Name"
+          helperText={errors.firstName?.message}
+          error={!!errors.firstName?.message}
+          fullWidth
+        />
+        <TextField
+          {...register("password")}
+          variant="outlined"
+          margin="normal"
+          label="Password"
+          helperText={errors.password?.message}
+          error={!!errors.password?.message}
+          type="password"
+          fullWidth
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={submitButton}
+        >
+          Sign Up
+        </Button>
+        {json && (
+          <>
+            <Typography variant="body1">
+              When the form is submitted, the following JSON data will be sent to the server.
+            </Typography>
+            <Typography variant="body2">{json}</Typography>
+          </>
+        )}
+      </form>
+    </Container>
+  );
+}
+
+export default App;
